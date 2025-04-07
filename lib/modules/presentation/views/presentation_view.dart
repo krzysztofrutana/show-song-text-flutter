220a5cf3be97948a504108be7cce46cc05c1de40
Krzysztof Rutana <krzysztofrutana@wp.pl>
2025-04-07 19:42:02 +0200

    Change own solution to great paginated_text library from dra11y

--- lib/modules/presentation/views/presentation_view.dart
+++ lib/modules/presentation/views/presentation_view.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_debouncer/flutter_debouncer.dart';
+import 'package:paginated_text/paginated_text.dart';
 import 'package:pomocnik_wokalisty/helpers/LocalStorage.dart';
 import 'package:pomocnik_wokalisty/modules/presentation/bloc/presentation_bloc.dart';
 import 'package:pomocnik_wokalisty/modules/songs/models/song_model.dart';
@@ -18,22 +18,59 @@ class PresentationView extends StatefulWidget {
 class _PresentationViewState extends State<PresentationView> {
   final Throttler _throttler = Throttler();
 
-  final List<String> _pageTexts = [];
-  int _currentIndex = 0;
   int _currentSongIndex = 0;
-  bool _needPaging = true;
-  bool _isPaging = false;
-  final _pageKey = GlobalKey();
-  final _rowKey = GlobalKey();
   late Song _currentSong;
   int _allSongsCount = 0;
+  late PaginatedController _controller;
+
+  bool _setLastPageFromSong = false;
 
   @override
   void initState() {
     _currentSong =
         context.read<PresentatationBloc>().state.songs[_currentSongIndex];
     _allSongsCount = context.read<PresentatationBloc>().state.songs.length;
+
+    _controller = _getController(_currentSong.text);
+
     super.initState();
+
+    _sendTextToClients(context);
+  }
+
+  PaginatedController _getController(String text) {
+    var fontSize = LocalStorage.instance.getInt('fontSize') ?? 15;
+
+    var controller = PaginatedController(PaginateData(
+      text: text,
+      dropCapLines: 0,
+      style: TextStyle(
+        color: Colors.black,
+        fontSize: fontSize.toDouble(),
+      ),
+      pageBreakType: PageBreakType.word,
+      breakLines: 1,
+      resizeTolerance: 3,
+      parseInlineMarkdown: true,
+    ));
+
+    controller.onPaginate = _onPaginate;
+
+    return controller;
+  }
+
+  _onPaginate(PaginatedController controller) {
+    if (_setLastPageFromSong) {
+      controller.setPageIndex(controller.numPages - 1);
+    }
+
+    _sendTextToClients(context);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   @override
@@ -42,93 +79,8 @@ class _PresentationViewState extends State<PresentationView> {
     super.deactivate();
   }
 
-  void _paginate() {
-    var pageSize =
-        (_pageKey.currentContext?.findRenderObject() as RenderBox).size;
-
-    final rowSize =
-        (_rowKey.currentContext?.findRenderObject() as RenderBox).size;
-
-    var appBarHeight =
-        Scaffold.of(_pageKey.currentContext!).appBarMaxHeight ?? 0;
-
-    _pageTexts.clear();
-
-    var fontSize = LocalStorage.instance.getInt('fontSize') ?? 15;
-
-    final textSpan = TextSpan(
-      text: _currentSong.text,
-      style: TextStyle(
-        color: Colors.black,
-        fontSize: fontSize.toDouble(),
-      ),
-    );
-    final textPainter = TextPainter(
-      text: textSpan,
-      textDirection: TextDirection.ltr,
-    );
-
-    textPainter.layout(
-      minWidth: 0,
-      maxWidth: pageSize.width,
-    );
-
-    // https://medium.com/swlh/flutter-line-metrics-fd98ab180a64
-    List<LineMetrics> lines = textPainter.computeLineMetrics();
-    int currentPageStartIndex = 0;
-    int currentPageEndIndex = 0;
-    double currentHeight = 0;
-
-    String pageText = '';
-    for (int i = 0; i < lines.length; i++) {
-      final line = lines[i];
-
-      final left = line.left;
-      final top = line.baseline - line.ascent;
-      // final bottom = line.baseline + line.descent;
-
-      // Current line overflow page
-      if (currentHeight + line.height > pageSize.height) {
-        // https://stackoverflow.com/questions/56943994/how-to-get-the-raw-text-from-a-flutter-textbox/56943995#56943995
-        currentPageEndIndex =
-            textPainter.getPositionForOffset(Offset(left, top)).offset;
-
-        final pageText = _currentSong.text
-            .substring(currentPageStartIndex, currentPageEndIndex);
-
-        _pageTexts.add(pageText);
-
-        currentPageStartIndex = currentPageEndIndex;
-        currentHeight = 0;
-      } else {
-        currentHeight += line.height;
-      }
-    }
-
-    final lastPageText = _currentSong.text.substring(currentPageStartIndex);
-    _pageTexts.add(lastPageText);
-
-    setState(() {
-      _currentIndex = 0;
-      _needPaging = false;
-      _isPaging = false;
-    });
-
-    _sendTextToClients(context);
-  }
-
   @override
   Widget build(BuildContext context) {
-    if (_needPaging && !_isPaging) {
-      _isPaging = true;
-
-      SchedulerBinding.instance.addPostFrameCallback((_) {
-        _paginate();
-      });
-    }
-
-    var fontSize = LocalStorage.instance.getInt('fontSize') ?? 15;
-
     return FocusScope(
       child: Focus(
         onKeyEvent: (node, event) {
@@ -136,9 +88,9 @@ class _PresentationViewState extends State<PresentationView> {
               duration: Duration(milliseconds: 200),
               onThrottle: () {
                 if (event.logicalKey == LogicalKeyboardKey.arrowRight) {
-                  if (_currentIndex < _pageTexts.length - 1) {
+                  if (!_controller.isLast) {
                     _handleNextPage(context);
-                  } else if (_currentIndex == _pageTexts.length - 1 &&
+                  } else if (_controller.isLast &&
                       _allSongsCount > 1 &&
                       _currentSongIndex < _allSongsCount - 1) {
                     _handleNextSong(context);
@@ -146,9 +98,9 @@ class _PresentationViewState extends State<PresentationView> {
 
                   return KeyEventResult.handled;
                 } else if (event.logicalKey == LogicalKeyboardKey.arrowLeft) {
-                  if (_currentIndex > 0) {
+                  if (!_controller.isFirst) {
                     _handlePreviousPage(context);
-                  } else if (_currentIndex == 0 &&
+                  } else if (_controller.isFirst &&
                       _allSongsCount > 1 &&
                       _currentSongIndex > 0) {
                     _handlePreviousSong(context, false);
@@ -178,30 +130,40 @@ class _PresentationViewState extends State<PresentationView> {
                   Text(_currentSong.title),
                   Visibility(
                       visible: _allSongsCount > 1,
-                      child: Text('${_currentSongIndex + 1}/$_allSongsCount'))
+                      child: Text('${_currentSongIndex + 1}/$_allSongsCount',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 18,
+                          )))
                 ],
               ),
               centerTitle: true),
-          body: SafeArea(
-            child: SizedBox.expand(
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(16.0, 0, 16.0, 0),
-                child: Stack(
-                  children: [
-                    Column(
-                      children: [
-                        Expanded(
-                          child: SizedBox.expand(
-                            key: _pageKey,
-                            child: Dismissible(
+          body: Padding(
+            padding: const EdgeInsets.all(16),
+            child: PaginatedText(
+              _controller,
+              builder: (context, child) {
+                var fontSize = LocalStorage.instance.getInt('fontSize') ?? 15;
+
+                return DefaultTextStyle(
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: fontSize.toDouble(),
+                  ),
+                  child: Column(
+                    children: [
+                      Expanded(
+                        child: SizedBox.expand(
+                          child: Dismissible(
                               resizeDuration: null,
-                              key: Key('$_currentSongIndex/$_currentIndex'),
+                              key: Key(
+                                  '$_currentSongIndex/${_controller.currentPage.pageIndex}'),
                               onDismissed: (DismissDirection direction) {
                                 // Swiping in left direction.
                                 if (direction == DismissDirection.startToEnd) {
-                                  if (_currentIndex > 0) {
+                                  if (!_controller.isFirst) {
                                     _handlePreviousPage(context);
-                                  } else if (_currentIndex == 0 &&
+                                  } else if (_controller.isFirst &&
                                       _allSongsCount > 1 &&
                                       _currentSongIndex > 0) {
                                     _handlePreviousSong(context, false);
@@ -209,88 +171,78 @@ class _PresentationViewState extends State<PresentationView> {
                                 }
                                 // Swiping in right direction.
                                 if (direction == DismissDirection.endToStart) {
-                                  if (_currentIndex < _pageTexts.length - 1) {
+                                  if (!_controller.isLast) {
                                     _handleNextPage(context);
-                                  } else if (_currentIndex ==
-                                          _pageTexts.length - 1 &&
+                                  } else if (_controller.isLast &&
                                       _allSongsCount > 1 &&
                                       _currentSongIndex < _allSongsCount - 1) {
                                     _handleNextSong(context);
                                   }
                                 }
                               },
-                              child: Text(
-                                _isPaging ? ' ' : _pageTexts[_currentIndex],
+                              child: child),
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          IconButton(
+                            icon: Icon(Icons.first_page),
+                            onPressed:
+                                _allSongsCount > 1 && _currentSongIndex > 0
+                                    ? () => _handlePreviousSong(context, true)
+                                    : null,
+                          ),
+                          Row(
+                            children: [
+                              IconButton(
+                                icon: Icon(Icons.first_page),
+                                onPressed: () {
+                                  _handleFirstPage(context);
+                                },
+                              ),
+                              IconButton(
+                                icon: Icon(Icons.navigate_before),
+                                onPressed: () {
+                                  _handlePreviousPage(context);
+                                },
+                              ),
+                              Text(
+                                _controller.numPages > 0
+                                    ? '${_controller.pageNumber}/${_controller.numPages}'
+                                    : '',
                                 style: TextStyle(
                                   color: Colors.black,
-                                  fontSize: fontSize.toDouble(),
+                                  fontSize: 18,
                                 ),
                               ),
-                            ),
+                              IconButton(
+                                icon: Icon(Icons.navigate_next),
+                                onPressed: () {
+                                  _handleNextPage(context);
+                                },
+                              ),
+                              IconButton(
+                                icon: Icon(Icons.last_page),
+                                onPressed: () {
+                                  _handleLastPage(context);
+                                },
+                              ),
+                            ],
                           ),
-                        ),
-                        Row(
-                          key: _rowKey,
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            IconButton(
-                              icon: Icon(Icons.first_page),
-                              onPressed:
-                                  _allSongsCount > 1 && _currentSongIndex > 0
-                                      ? () => _handlePreviousSong(context, true)
-                                      : null,
-                            ),
-                            Row(
-                              children: [
-                                IconButton(
-                                  icon: Icon(Icons.first_page),
-                                  onPressed: () {
-                                    _handleFirstPage(context);
-                                  },
-                                ),
-                                IconButton(
-                                  icon: Icon(Icons.navigate_before),
-                                  onPressed: () {
-                                    _handlePreviousPage(context);
-                                  },
-                                ),
-                                Text(
-                                  _isPaging
-                                      ? ''
-                                      : '${_currentIndex + 1}/${_pageTexts.length}',
-                                ),
-                                IconButton(
-                                  icon: Icon(Icons.navigate_next),
-                                  onPressed: () {
-                                    _handleNextPage(context);
-                                  },
-                                ),
-                                IconButton(
-                                  icon: Icon(Icons.last_page),
-                                  onPressed: () {
-                                    _handleLastPage(context);
-                                  },
-                                ),
-                              ],
-                            ),
-                            IconButton(
-                              icon: Icon(Icons.last_page_sharp),
-                              onPressed: _allSongsCount > 1 &&
-                                      _currentSongIndex < _allSongsCount - 1
-                                  ? () => _handleNextSong(context)
-                                  : null,
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                    if (_isPaging)
-                      Center(
-                        child: CircularProgressIndicator(),
+                          IconButton(
+                            icon: Icon(Icons.last_page_sharp),
+                            onPressed: _allSongsCount > 1 &&
+                                    _currentSongIndex < _allSongsCount - 1
+                                ? () => _handleNextSong(context)
+                                : null,
+                          ),
+                        ],
                       ),
-                  ],
-                ),
-              ),
+                    ],
+                  ),
+                );
+              },
             ),
           ),
         ),
@@ -300,7 +252,7 @@ class _PresentationViewState extends State<PresentationView> {
 
   void _handleFirstPage(BuildContext context) {
     setState(() {
-      _currentIndex = 0;
+      _controller.setPageIndex(0);
     });
 
     _sendTextToClients(context);
@@ -308,16 +260,16 @@ class _PresentationViewState extends State<PresentationView> {
 
   void _handleLastPage(BuildContext context) {
     setState(() {
-      _currentIndex = _pageTexts.length - 1;
+      _controller.setPageIndex(_controller.numPages - 1);
     });
 
     _sendTextToClients(context);
   }
 
   void _handlePreviousPage(BuildContext context) {
-    if (_currentIndex > 0) {
+    if (!_controller.isFirst) {
       setState(() {
-        _currentIndex--;
+        _controller.previous();
       });
 
       _sendTextToClients(context);
@@ -329,20 +281,16 @@ class _PresentationViewState extends State<PresentationView> {
       _currentSongIndex -= 1;
       _currentSong =
           context.read<PresentatationBloc>().state.songs[_currentSongIndex];
-      _paginate();
+      _controller = _getController(_currentSong.text);
 
-      if (!returnToFirstIndex) {
-        _currentIndex = _pageTexts.length - 1;
-      }
+      if (!returnToFirstIndex) _setLastPageFromSong = true;
     });
-
-    _sendTextToClients(context);
   }
 
   void _handleNextPage(BuildContext context) {
-    if (_currentIndex < _pageTexts.length - 1) {
+    if (!_controller.isLast) {
       setState(() {
-        _currentIndex++;
+        _controller.next();
       });
       _sendTextToClients(context);
     }
@@ -353,15 +301,13 @@ class _PresentationViewState extends State<PresentationView> {
       _currentSongIndex += 1;
       _currentSong =
           context.read<PresentatationBloc>().state.songs[_currentSongIndex];
-      _paginate();
+      _controller = _getController(_currentSong.text);
     });
-
-    _sendTextToClients(context);
   }
 
   void _sendTextToClients(BuildContext context) {
     if (context.read<ServerCubit>().state.server.serverStarted) {
-      context.read<ServerCubit>().send(_pageTexts[_currentIndex]);
+      context.read<ServerCubit>().send(_controller.currentPage.text);
     }
   }
 }
